ae929ea506981899961d34180a7eba127c7c6045
Natalia Shaurina <natashayrina@mail.ru>
2023-01-20 17:55:58 +0300

    Добавила функцию определения взрослости животного.

--- Functions.py
+++ Functions.py
@@ -3,18 +3,19 @@ from Dictionaris import *
 
 
 def main(animals_all):
-    choice = 0
+    choice: int = 0
     while choice != quit:
-        choice = get_menu_choice()
+        var = choice == get_menu_choice()
         if choice == look_for_type_of_animal:
-            result = search_type_of_animal(animals_all)
+            result = search_type_of_animal(choice, animals_all)
 
         elif choice == look_for_owner:
             result = search_owner(animals_all)
-        print_of_animal(result)
 
-        # elif choice == look_for_age:
-        #     search_age(animais_all)
+        elif choice == look_for_age:
+            result = search_age(ages, animals_all)
+
+        print_of_animal(result)
 
 
 def get_need_type():
@@ -39,3 +40,15 @@ def search_owner(animals_all):
         if elem['owner'] == owner:
             owner_list. append(elem)
     return owner_list
+
+def search_age(ages, animals_all):
+    ages = get_adult()
+    for elem in animals_all:
+        if ages == elem ['is_adult' :'True']:
+            print("Животное взрослое")
+        else:
+            print("Животное еще молодое")
+
+
+
+
